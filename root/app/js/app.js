43761adb7f55cb5d6012c7095b28947c79518d7e
LAU Thierry <thierlau@gmail.com>
2014-02-11 09:17:33 +0100

    move sample controller out of  app.js

--- root/app/js/app.js
+++ root/app/js/app.js
@@ -3,11 +3,15 @@ angular.module('{%= name %}', {%= importedModules %});
 {%if (importedModules.indexOf('ui.router') !== -1 && importedModules.indexOf('pascalprecht.translate') === -1) {%}
 angular.module('{%= name %}').config(function($stateProvider, $urlRouterProvider) {
 
+    "use strict";
+
 });
 {%}%}
 
 {%if (importedModules.indexOf('pascalprecht.translate') !== -1 && importedModules.indexOf('ui.router') === -1) {%}
 angular.module('{%= name %}').config(function($translateProvider) {
+
+    "use strict";
 
 });
 {%}%}
@@ -15,11 +19,7 @@ angular.module('{%= name %}').config(function($translateProvider) {
 {%if (importedModules.indexOf('ui.router') !== -1 && importedModules.indexOf('pascalprecht.translate') !== -1) {%}
 angular.module('{%= name %}').config(function($stateProvider, $urlRouterProvider, $translateProvider) {
 
-});
-{%}%}
-
-angular.module('{%= name %}').controller('mainController', function ($scope) {
     "use strict";
 
-    $scope.message = "Yeahhh ! You're ready !";
 });
+{%}%}
